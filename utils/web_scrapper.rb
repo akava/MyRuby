dff632bd9300fd61575cf75f7980c33c4ccefa3a
AKavaleu <none@none>
2011-08-21 17:30:33 +0200

    Ruby Hard way scrapped

--- utils/web_scrapper.rb
+++ utils/web_scrapper.rb
@@ -1,14 +1,24 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-require 'UniversalDetector'
+require 'rio'
 
-output = ""
 
-range = (40458..40509)
-# range = [40458]
+start_url = "http://ruby.learncodethehardway.org/"
 
-File.open("Honor.txt", 'w') do |f|
+doc = Nokogiri::HTML(open(start_url))
+doc.xpath(("/html/body/div/section/section/ul/li/a")).each do |chapter_node|
+  #<a href="/intro.html">The Hard Way Is Easier</a>
+  chapter_url = start_url + chapter_node['href'][1..100]
+  chapter_title = chapter_node.inner_text.gsub(/:/) {  }
+
+  p " Saving chapter '#{chapter_title}' (#{chapter_url})"
+  rio(chapter_url) > rio(chapter_title + '.html')
+
+end
+
+
+=begin
   range.each  do |id|
     url =   "http://notabenoid.com/book/12477/#{id}/ready?algorithm=0&untr=o&format=h"
     p url
@@ -25,3 +35,5 @@ File.open("Honor.txt", 'w') do |f|
     end
   end
 end
+
+=end
