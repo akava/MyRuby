0f080101a5abb358cbe9995a566376181a3aea6c
AKavaleu <none@none>
2011-10-12 09:53:09 +0300

    Parser now reads all resopurce languages

--- utils/resource_parser.rb
+++ utils/resource_parser.rb
@@ -3,14 +3,15 @@ require 'nokogiri'
 require 'resx_template'
 
 ROOT_DIR = 'c:/Andrew/IBA/GDYR/MESPOD/SVN_HG/MESPOD/'
-TO_TRANSLATE_CSV =  'MessagesToTranslate.csv'
-TRANSLATED_CSV = 'TranslatedMessages.csv'
+TO_TRANSLATE_CSV =  '../../MessagesToTranslate.csv'
+TRANSLATED_CSV = '../../TranslatedMessages.csv'
 TO_TRANSLATE_CSV_MERGED = 'MessagesToTranslate_merged.csv'
 
 def resources_to_csv
   Dir::chdir(ROOT_DIR)
-  hash = Hash.new()
+  def_lang_list = Hash.new()
 
+  # create default (EN) messages list
   Dir['**/*.resx'].each do |f_name|
     next if f_name.end_with?('de-DE.resx', 'de.resx', 'sl.resx')
 
@@ -21,19 +22,59 @@ def resources_to_csv
         name = node['name']
         value = node.children()[1].inner_text
 
-        hash[f_name] = [] if not hash.has_key?(f_name)
-        hash[f_name].push([name, value])  if name.end_with?("Caption", "Text") or f_name.end_with?("UserMessage.resx", "Resources.resx")
+        def_lang_list[f_name] = [] if not def_lang_list.has_key?(f_name)
+        def_lang_list[f_name].push([name, value])  if (name.end_with?("Caption", "Text"))or f_name.end_with?("UserMessage.resx", "Resources.resx")
       end
     end
   end
 
-  File.open(TO_TRANSLATE_CSV, "w") do |f|
-    hash.each_pair do |f_name, val|
-      val.each do |(name, value)|
-        f.write("#{f_name};#{name};#{value}\n")
+  other_lang_list = Hash.new()
+  # combine default list with other languages translations
+  res_lang_regexp = /\/\S+\.([^\.]+)\.resx/
+  Dir['**/*.resx'].each do |f_name|
+    next if def_lang_list.has_key?(f_name)
+
+    match = res_lang_regexp.match(f_name)
+    next if match.nil?
+
+    lang = match[1]
+    def_lang_f_name = f_name.sub(/([^\.]+)\.resx/, "resx")
+    File.open(f_name) do |f|
+      doc = Nokogiri::XML(f)
+
+      doc.xpath(("//data[not(@type)]")).each do |node|
+        name = node['name']
+        value = node.children()[1].inner_text
+
+        other_lang_list[def_lang_f_name + "|" + name] = {} if not def_lang_list.has_key?(def_lang_f_name + "|" + name)
+        other_lang_list[def_lang_f_name + "|" + name][lang] = value  if (name.end_with?("Caption", "Text"))
       end
     end
   end
+
+  merged_list = {}
+  # merging two lists
+  def_lang_list.each_pair do |f_name, val|
+    val.each do |(name, value)|
+      langs_map =  other_lang_list[f_name + "|" + name]
+      langs_map = {} if langs_map.nil?
+      langs_map[:default] = value
+
+      merged_list[f_name] = [] if merged_list[f_name].nil?
+      merged_list[f_name].push([name, langs_map])
+    end
+  end
+
+
+  File.open(TO_TRANSLATE_CSV, "w") do |f|
+    f.write("File name;Resource name;EN;SL\n")
+    merged_list.each_pair do |f_name, val|
+      val.each do |(name, langs_map)|
+        f.write("#{f_name};#{name};#{langs_map[:default]};#{langs_map["sl"]}\n")
+      end
+    end
+  end
+  return 0
 end
 
 def csv_to_resources
@@ -66,8 +107,7 @@ def append_translation
     Dir::chdir(ROOT_DIR)
   File.open(TRANSLATED_CSV) do |f|
     while (line = f.gets)
-      (res_f, msg_id, msg, trans) = line.split(/;/)
-      id = res_f + msg_id
+      (_, _, msg, trans) = line.split(/;/)
 
       hash_by_msg[msg]=trans
     end
@@ -77,7 +117,7 @@ def append_translation
   File.open(TO_TRANSLATE_CSV_MERGED, "w") do |fout|
      while (line = fin.gets)
        line.sub!(/\n/,"")
-       (res_f, msg_id, msg) = line.split(/;/)
+       (_, _, msg) = line.split(/;/)
        trans = ""
 
        trans = hash_by_msg[msg] if hash_by_msg.has_key?(msg)
